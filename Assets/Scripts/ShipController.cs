fb0e28d4fb7aa41c5cd18da67dfd7fbb9eb4b8b2
Elias <eliasbrucart@hardgames.com.ar>
2021-06-21 20:42:07 -0300

    Ship can fire

---
+++ Assets/Scripts/ShipController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ShipController : MonoBehaviour
+{
+    [SerializeField] private GameObject bullet;
+    [SerializeField] private float shootSpeed;
+    [SerializeField] private Transform shotSpawn;
+    [SerializeField] private float fireRate;
+    private float nextFire;
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        Shoot();
+    }
+
+    void Shoot()
+    {
+        if (Input.GetKey(KeyCode.E) && Time.time > nextFire)
+        {
+            nextFire = Time.time + fireRate;
+            Instantiate(bullet, shotSpawn.position, Quaternion.identity);
+        }
+    }
+}
