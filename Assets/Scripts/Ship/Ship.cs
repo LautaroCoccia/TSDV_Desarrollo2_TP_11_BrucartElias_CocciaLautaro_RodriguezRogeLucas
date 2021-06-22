b8528ff6e82244bfb25ee126fb40d6bcbff9e7a1
Elias <eliasbrucart@hardgames.com.ar>
2021-06-22 18:05:28 -0300

    Added basic FSM

--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -23,16 +23,5 @@ public class Ship : MonoBehaviour
         float x = Input.GetAxis("Horizontal");
         float y = Input.GetAxis("Vertical");
         rb.velocity = new Vector2(x, y) * speed;
-        //transform.position += new Vector3(x, y, 0) * speed * Time.deltaTime;
-        //if (transform.position.x >= -8 && transform.position.x <= 8 && transform.position.y <= 3.70f && transform.position.y >= -3.70f)
-        /*if(transform.position.x <= 8)
-            if (Input.GetKey(KeyCode.RightArrow))
-                transform.position += Vector3.right * speed * Time.deltaTime;
-        if(transform.position.y <= 3.70f)
-            if (Input.GetKey(KeyCode.UpArrow))
-                transform.position += Vector3.up * speed * Time.deltaTime;
-        if(transform.position.y >= -3.70f)
-            if (Input.GetKey(KeyCode.DownArrow))
-                transform.position += Vector3.down * speed * Time.deltaTime;*/
     }
 }
