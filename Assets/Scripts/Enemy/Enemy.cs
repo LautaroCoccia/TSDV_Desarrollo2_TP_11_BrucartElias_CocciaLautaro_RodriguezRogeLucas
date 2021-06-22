c0509cdb8d33bd1fc0fb5c78808b7090c286cbb8
Elias <eliasbrucart@hardgames.com.ar>
2021-06-22 12:54:24 -0300

    New way to move the player

---
+++ Assets/Scripts/Enemy/Enemy.cs
@@ -0,0 +1,16 @@
+﻿using UnityEngine;
+
+public class Enemy : MonoBehaviour
+{
+    [SerializeField] private int lives { get; set; }
+
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
--- Assets/Scripts/Ship/Ship.cs
+++ Assets/Scripts/Ship/Ship.cs
@@ -6,23 +6,33 @@ using UnityEngine;
 public class Ship : MonoBehaviour
 {
     [SerializeField] private float speed;
-    void Start()
+    private Rigidbody2D rb;
+
+    private void Start()
     {
-        
+        rb = GetComponent<Rigidbody2D>();
     }
 
-    void Update()
+    void FixedUpdate()
     {
         MoveShip();
     }
 
     void MoveShip()
     {
-        if(transform.position.x >= -8)
-            if (Input.GetKey(KeyCode.LeftArrow))
-                transform.position += Vector3.left * speed * Time.deltaTime;
-        if(transform.position.x <= 8)
+        float x = Input.GetAxis("Horizontal");
+        float y = Input.GetAxis("Vertical");
+        rb.velocity = new Vector2(x, y) * speed;
+        //transform.position += new Vector3(x, y, 0) * speed * Time.deltaTime;
+        //if (transform.position.x >= -8 && transform.position.x <= 8 && transform.position.y <= 3.70f && transform.position.y >= -3.70f)
+        /*if(transform.position.x <= 8)
             if (Input.GetKey(KeyCode.RightArrow))
                 transform.position += Vector3.right * speed * Time.deltaTime;
+        if(transform.position.y <= 3.70f)
+            if (Input.GetKey(KeyCode.UpArrow))
+                transform.position += Vector3.up * speed * Time.deltaTime;
+        if(transform.position.y >= -3.70f)
+            if (Input.GetKey(KeyCode.DownArrow))
+                transform.position += Vector3.down * speed * Time.deltaTime;*/
     }
 }
