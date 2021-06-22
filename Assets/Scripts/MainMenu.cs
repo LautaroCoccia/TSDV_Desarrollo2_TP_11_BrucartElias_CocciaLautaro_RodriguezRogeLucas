512960f3e772050110d657bf3b996c0b66403d91
Gaiusglaber <lucas.rodriguez.roge@gmail.com>
2021-06-22 01:59:00 -0300

    MainMenu scene added

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject creditsMenu;
+    public void Play() 
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+    public void Exit()
+    {
+        Application.Quit();
+    }
+    public void Credits()
+    {
+        mainMenu.SetActive(false);
+        creditsMenu.SetActive(true);
+    }
+    public void GoBack()
+    {
+        mainMenu.SetActive(true);
+        creditsMenu.SetActive(false);
+    }
+}
